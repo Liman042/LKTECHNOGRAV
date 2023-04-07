363f503f66bea0905863e59ee4e092421a3d97e9
Fuad <fuad-9696@mail.ru>
2023-04-07 21:09:54 +0300

    Привязали действие к кнопке через код

--- LKTECHNOGRAV/ViewController.swift
+++ LKTECHNOGRAV/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var helloWorldLabel: UILabel!
+    
+    @IBOutlet var showButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        helloWorldLabel.isHidden = true
+        showButton.layer.cornerRadius = 20
+    
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func makeButtonAction(_ sender: Any) {
+        if helloWorldLabel.isHidden {
+            helloWorldLabel.isHidden = false
+            showButton.setTitle("Hight text", for: .normal)
+        } else {
+            helloWorldLabel.isHidden = true
+            showButton.setTitle("Show text", for: .normal)
+        }
+    }
+    
 
 }
 
